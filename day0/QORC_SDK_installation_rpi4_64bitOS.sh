f91e0a1a068f157c7a66c58a61386e49f8fc3d17
Farhan Rasheed <frasheed.dev@gmail.com>
2022-08-03 16:11:20 +0200

    QORC installation file

---
+++ day0/QORC_SDK_installation_rpi4_64bitOS.sh
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------------------------------------------------
+# Description	: Setup file for installing SDK for the Quick Logic EOS-S3 board (for Raspberry Pi OS based systems).
+# Author		: Farhan Rasheed
+# Email			: frasheed.dev@gmail.com
+# GitHub		: https://github.com/frasheed-dev/
+#-------------------------------------------------------------------------------------------------------------------------
+
+# BASIC INFORMATION
+# -----------------
+
+# QORC SDK
+# ========
+# QuickLogic Open Reconfigurable Computing (QORC) SDK provides components
+# needed to get started on the QuickLogic's EOSS3 device and open source
+# development boards such as Quickfeather.
+# Further Details:
+# [1] LINK
+# [2] LINK
+
+# Supportted Boards
+# =================
+# As of August 2022, following hardware boards are supported:
+#-  `Quickfeather Development Kit <https://www.quicklogic.com/products/eos-s3/quickfeather-development-kit/>`__
+#-  `SparkFun Thing Plus <https://www.quicklogic.com/products/eos-s3/sparkfun-thing-plus/>`__ (Board used in this work)
+#-  `Qomu Development Kit <https://www.quicklogic.com/products/eos-s3/qomu-dev-kit/>`__
+
+# INSTALLATION
+# ------------
+
+# Official Installation Guide: LINK
+
+# Since there is a detailed guide available on how to install the QORC SDK, I will trim it to my usecase for the following Hardware/Software components:
+#	Hardware: Raspberry Pi 4 Model B Rev 1.4 (4GB RAM) 
+#	Software: Raspberry Pi OS (64-bit)  
+#	uname -a: Linux pi4 5.15.32-v8+ #1538 SMP PREEMPT Thu Mar 31 19:40:39 BST 2022 aarch64 GNU/Linux
+#	FPGA	: SparkFun Thing Plus (QuickLogic EOS S3)
+
